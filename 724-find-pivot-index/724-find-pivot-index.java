0bc8cf2073ebc9d3f60238923efbd9a6718e5dc2
Pratik Joshi <85014122+pratikjoshi22@users.noreply.github.com>
2022-07-20 22:47:07 +0530

    Time: 2 ms (78.80%), Space: 52.6 MB (39.60%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+       
+        //Arrays.sort(nums);
+           int total = 0;
+        int sum_till = 0;
+		
+		// total sum
+        for(int val: nums) total += val;
+        
+        for(int i=0;i<nums.length;i++) {
+			// if sum till now (left sum), is equal to total - sum till now - (means right sum)
+            if(sum_till == total - sum_till - nums[i]) return i;
+            sum_till += nums[i];
+        }
+        return -1;
+        
+    }
+}
