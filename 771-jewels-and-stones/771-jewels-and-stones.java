5b949f650d0d9d5463e542cfc79015b9b654a8bc
Pratik Joshi <85014122+pratikjoshi22@users.noreply.github.com>
2022-07-20 20:37:43 +0530

    Time: 2 ms (59.44%), Space: 41.8 MB (78.16%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.java
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -1,48 +1,11 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int low[] = new int[26];
-        int alph[] = new int[26];
-        for(int i =0;i<26;i++)
+        int ans = 0;
+        for(int i=0;i<jewels.length();i++)
         {
-            low[i] = -1;
-            alph[i] = -1;
-            
-        }
-        // low[2] = 3;
-        for(int i = 0;i<jewels.length();i++)
-        {
-            if(jewels.charAt(i)>='a' && jewels.charAt(i)<='z' )
+            for(int j=0;j<stones.length();j++)
             {
-                low[jewels.charAt(i)-'a']=0;
-            }
-            else
-            {
-                alph[jewels.charAt(i)-'A']=0;
-            }
-                
-        }
-        int ans=0;
-        for(int i=0;i<stones.length();i++)
-        {
-            // System.out.print(stones.charAt(i)-'a' + " ");
-            // System.out.print(alph[0] == -1);
-            // int lowcal = stones.charAt(i)-'a;
-            // int alphcal = stones.charAt(i)-'A';
-
-            // if(low[stones.charAt(i)-'a'] == 0 )//|| alph[stones.charAt(i)-'A'] == 0 )
-            // {
-            //     ans++;
-            // }
-            if(stones.charAt(i)>='a' && stones.charAt(i)<='z' )
-            {
-                if(low[stones.charAt(i)-'a'] == 0 )//|| alph[stones.charAt(i)-'A'] == 0 )
-                {
-                    ans++;
-                }
-            }
-            else
-            {
-                if(alph[stones.charAt(i)-'A'] == 0 )//|| alph[stones.charAt(i)-'A'] == 0 )
+                if(jewels.charAt(i)==stones.charAt(j))
                 {
                     ans++;
                 }
